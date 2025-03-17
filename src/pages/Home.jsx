a02f0899b5581d39fe3b70bf48318a47d25f2688
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-17 15:38:52 +0100

    add: home content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <>
-      <h1 className="font-h1">Titolo Homepage</h1>
+      <main className='container-fluid'>
+        {/* hero */}
+        <section className='perfect-center p-5u flex min-h-[28vh] rounded-xl border-[12px] border-white bg-indigo-50'>
+          <h1 className='font-h1'>
+            Leggi le <span className='text-indigo-400'>recensioni</span> dei
+            film
+          </h1>
+        </section>
+      </main>
     </>
   );
 }
