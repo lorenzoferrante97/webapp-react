f067e399ffb70ba5ccd643e12518bc7767916b56
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-17 11:54:00 +0100

    add: starting app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
-function App() {}
+import { GlobalProvider } from "./contexts/GlobalContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // router
+import DefaultLayout from "./layouts/DefaultLayout"; // layouts
+import Home from "./pages/Home"; // pages
+
+function App() {
+  return (
+    <GlobalProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={DefaultLayout}>
+            <Route path="/" Component={Home}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </GlobalProvider>
+  );
+}
 
 export default App;
+
