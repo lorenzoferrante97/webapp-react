330489753f8bd4c1038036f8c17e68b71362f5fe
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-18 11:35:25 +0100

    add: fetch api

--- src/contexts/GlobalContext.jsx
+++ src/contexts/GlobalContext.jsx
@@ -6,7 +6,27 @@ const GlobalProvider = ({ children }) => {
   const [movies, setMovies] = useState([]);
   const [movie, setMovie] = useState({});
 
-  const value = {};
+  // fetch movies
+  const fetchMovies = () => {
+    fetch("http://localhost:3000/movies")
+      .then(response => response.json())
+      .then(data => setMovies(data))
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  // fetch movie
+  const fetchMovie = id => {
+    fetch(`http://localhost:3000/movies/${id}`)
+      .then(response => response.json())
+      .then(data => setMovie(data))
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  const value = { movies, movie };
 
   return (
     <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>
