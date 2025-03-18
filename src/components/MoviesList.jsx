d2ba8d526ab2845b40977f4920999fa4e04e4778
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-18 10:55:57 +0100

    add: movies list component

---
+++ src/components/MoviesList.jsx
@@ -0,0 +1,9 @@
+export default function MoviesList() {
+  return (
+    <>
+      <section className='row-grid'>
+        <div className='col-span-full text-center'>Movies List</div>
+      </section>
+    </>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import MoviesList from "../components/MoviesList";
+
 export default function Home() {
   return (
     <>
-      <main className='container-fluid'>
+      <main className='container-fluid gap-36u'>
         {/* hero */}
         <section className='perfect-center p-5u flex min-h-[28vh] rounded-xl border-[12px] border-white bg-indigo-50'>
           <h1 className='font-h1'>
@@ -9,6 +11,8 @@ export default function Home() {
             film
           </h1>
         </section>
+        {/* movies list */}
+        <MoviesList />
       </main>
     </>
   );
