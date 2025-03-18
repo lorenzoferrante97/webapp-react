94072495c2546523e82caee8ec370619022558c6
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-18 10:58:41 +0100

    add: movies & movie global variables

--- src/contexts/GlobalContext.jsx
+++ src/contexts/GlobalContext.jsx
@@ -1,11 +1,16 @@
-import { createContext, useState, useContext } from 'react';
+import { createContext, useState, useContext } from "react";
 
 const GlobalContext = createContext();
 
 const GlobalProvider = ({ children }) => {
+  const [movies, setMovies] = useState([]);
+  const [movie, setMovie] = useState({});
+
   const value = {};
 
-  return <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>;
+  return (
+    <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>
+  );
 };
 
 const useGlobalContext = () => useContext(GlobalContext);
